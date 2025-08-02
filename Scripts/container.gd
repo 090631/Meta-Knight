440a9a0619f01f1f3cd2ae787b6e6c99bfe88883
Adi Zain <habid6338@gmail.com>
2025-08-02 22:59:04 +0600

    Hud Addition

---
+++ Scripts/container.gd
@@ -0,0 +1,6 @@
+extends Container
+@onready var player: CharacterBody2D = %Player
+@onready var health: Label = $Health
+
+func  _process(delta):
+	health.text = "Health: " + str(player.health) 
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 @onready var player: CharacterBody2D = %Player
-@onready var health: Label = $Health
+@onready var health: Label = $Control/Health
+@onready var hud: CanvasLayer = $"."
 
+	
 func  _process(delta):
 	health.text = "Health: " + str(player.health) 
+	hud.follow_viewport_enabled = false
